c033e02e8ce9a5edb7e576a93940bacc2b2eae5c
Mysteryman33 <139012459+Mysteryman33@users.noreply.github.com>
2026-03-04 19:03:40 -0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Load API key from environment (Render/Railway/Replit)
-HF_API_KEY = os.getenv("HF_API_KEY")
+HF_API_KEY = os.getenv("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")
 MODEL = "meta-llama/Llama-3.2-1B-Instruct"
 
 # Normal mode styles
@@ -215,3 +215,4 @@ def chat():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
